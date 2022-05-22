53dfa3c09d25b28e707363026fe998759387083b
Bennington <caleb.welch11@gmail.com>
2022-05-21 22:46:22 -0400

    edit todo works

--- todo/src/App.js
+++ todo/src/App.js
@@ -6,7 +6,8 @@ import TodosList from './components/TodosList';
 
 function App() {
   const [input, setInput] = useState("");
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
+  const [editTodo, setEditTodo] = useState(null);
   return (
     <div className="App">
       <Header />
@@ -15,8 +16,10 @@ function App() {
         setInput={setInput}
         todos={todos}
         setTodos={setTodos}
+        editTodo={editTodo}
+        setEditTodo={setEditTodo}
       />
-      <TodosList todos={todos} setTodos={setTodos} />
+      <TodosList todos={todos} setTodos={setTodos} setEditTodo={setEditTodo} />
     </div>
   );
 }
--- todo/src/components/TodosList.js
+++ todo/src/components/TodosList.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
-const TodosList = ({ todos, setTodos }) => {
+const TodosList = ({ todos, setTodos, setEditTodo }) => {
+
+    const handleEditTodo = ({ id }) => {
+        const findTodo = todos.find((todo) => todo.id === id);
+        setEditTodo(findTodo);
+    }
+
+    const handleDelete = ({ id }) => {
+        setTodos(todos.filter((todo) => todo.id !== id));
+    };
+
+    const handleComplete = (todo) => {
+        setTodos(
+            todos.map((item) => {
+                if (item.id === todo.id) {
+                    return { ...item, completed: !item.completed }
+                }
+                return item;
+            })
+        );
+    };
+
     return (
         <div>
             {todos.map((todo) => (
@@ -10,6 +31,17 @@ const TodosList = ({ todos, setTodos }) => {
                     key={todo.id}
                 >
                     <input type="text" value={todo.title} className="list" onChange={(event) => event.preventDefault()} />
+                    <div>
+                        <button onClick={() => handleComplete(todo)}>
+                            complete
+                        </button>
+                        <button onClick={() => handleEditTodo(todo)}>
+                            edit
+                        </button>
+                        <button onClick={() => handleDelete(todo)}>
+                            remove
+                        </button>
+                    </div>
                 </li>
             ))}
         </div>
